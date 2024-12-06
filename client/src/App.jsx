478a800cdc5d7f6ff7cbfe4d05320fa4b1604c8a
githubscientist <sathish.datascientist@gmail.com>
2024-12-06 20:34:24 +0530

    useEffect --with dependency list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,25 @@ import { useEffect, useState } from "react";
 const App = () => {
 
   const [count, setCount] = useState(0);
+  const [likes, setLikes] = useState(0);
 
   // runs when the component renders and only once
   useEffect(() => {
     console.log(count);
-  }, []);
+  }, [count]);
 
   return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-    </div>
+    <>
+      <div>
+        <h1>Count: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+      </div>
+
+      <div>
+        <h1>Likes: {likes}</h1>
+        <button onClick={() => setLikes(likes + 1)}>Like</button>
+      </div>
+    </>
   )
 }
 
