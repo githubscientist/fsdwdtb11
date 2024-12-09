6e2f5317e979339ffb43497ef5bec7b96a1cf37f
githubscientist <sathish.datascientist@gmail.com>
2024-12-09 20:22:32 +0530

    useReducer hook --counter app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
+// define the initial state
+const initialState = {
+  count: 0
+}
+
+// define the reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        ...state,
+        count: state.count - 1
+      }
+    default:
+      return state
+  }
+}
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Counter: {state.count}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
     </div>
   )
 }
