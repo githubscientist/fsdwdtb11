ce920f7cea9b6b1c6df7e50aeb3940d16b50c642
githubscientist <sathish.datascientist@gmail.com>
2024-12-11 19:40:47 +0530

    react router setup + warnings cleaned

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const App = () => {
 
-  const router = createBrowserRouter([
+  const routes = [
     {
       path: '/',
       element: <h1>Hello World!</h1>
@@ -11,9 +11,25 @@ const App = () => {
       path: "/contact",
       element: <h1>Contact Us</h1>
     }
-  ])
+  ];
 
-  return <RouterProvider router={router} />
+  const router = createBrowserRouter(routes, {
+    future: {
+      v7_normalizeFormMethod: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+      v7_skipActionStatusRevalidation: true
+    }
+  });
+
+  return <RouterProvider
+    router={router}
+    future={{
+      v7_startTransition: true,
+    }}
+  />
 }
 
 export default App;
