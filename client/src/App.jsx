2532360dc29e0a123016ad34f9d2e966047cccfa
githubscientist <sathish.datascientist@gmail.com>
2024-12-12 19:15:56 +0530

    axios api call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <div>App</div>
   )
