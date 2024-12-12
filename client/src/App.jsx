026c18e75c14c2744458df80d02a013dfc13dba7
githubscientist <sathish.datascientist@gmail.com>
2024-12-12 20:48:01 +0530

    state management using useReducer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react"
+import { useReducer, useState } from "react"
+
+const initialState = {
+  count: 0
+}
+
+const reducer = (state, action) => {
+  if (action.type === 'PLUS') {
+    return {
+      count: state.count + 1
+    }
+  } else if (action.type === 'MINUS') {
+    return {
+      count: state.count - 1
+    }
+  }
+  return state;
+}
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handlePlus = () => {
-    setCount(count + 1);
+    dispatch({ type: "PLUS" });
   }
 
   const handleMinus = () => {
-    setCount(count - 1);
+    dispatch({ type: "MINUS" });
   }
 
   return (
     <div>
-      <button onClick={handleMinus}>-</button><p style={{ display: "inline" }}> {count} </p><button onClick={handlePlus}>+</button>
+      <button onClick={handleMinus}>-</button><p style={{ display: "inline" }}> {state.count} </p><button onClick={handlePlus}>+</button>
     </div>
   )
 }
