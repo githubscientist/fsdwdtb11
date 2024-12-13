9d64c448c82fd5607f9217f0bf1a08c8726120c6
githubscientist <sathish.datascientist@gmail.com>
2024-12-13 21:13:34 +0530

    exercise --todo list

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 const Todos = () => {
 
     const todos = useLoaderData();
 
+    const [status, setStatus] = useState('all');
+
     return (
         <div>
             <h1>Todo List</h1>
+            <select
+                value={status}
+                onChange={e => setStatus(e.target.value)}
+            >
+                <option value='all'>All</option>
+                <option value='completed'>Completed</option>
+                <option value='incomplete'>Incomplete</option>
+            </select>
             <ul>
                 {
-                    todos.map(todo => <li key={todo.id}>{todo.title}</li>)
+                    todos
+                        .filter(todo => {
+                            if (status === 'all') {
+                                return true;
+                            } else if (status === 'completed') {
+                                return todo.completed;
+                            } else {
+                                return !todo.completed;
+                            }
+                        })
+                        .map(todo => <li key={todo.id}>{todo.title}</li>)
                 }
             </ul>
-        </div>
+        </div >
     )
 }
 
