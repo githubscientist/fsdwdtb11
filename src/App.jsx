d6da47e8f25c92bd0ce83dd5840eab2aeabe2db4
githubscientist <sathish.datascientist@gmail.com>
2024-12-17 19:12:14 +0530

    react router setup --done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+];
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true
+  }
+})
+
 const App = () => {
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <p>Hello World</p>
-    </>
-  )
+  return <RouterProvider
+    router={router}
+    future={{
+      v7_startTransition: true,
+    }}
+  />
 }
 
 export default App;
