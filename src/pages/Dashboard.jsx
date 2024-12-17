5cdd967b9ae172479dbe2e7768717ec7aa3990c9
githubscientist <sathish.datascientist@gmail.com>
2024-12-17 20:52:58 +0530

    using useEffect --api fetch

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+
 const Dashboard = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://676195b946efb37323724255.mockapi.io/jobs`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setJobs(data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
+
+    console.log(jobs);
+
     return (
         <div>Dashboard</div>
     )
